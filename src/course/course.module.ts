9b4694bfdd4b4870d289923c02c9abb4447bb4d1
leandro <leandro.ecabello@gmail.com>
2024-05-30 01:59:11 -0300

    Added course service and controller

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CourseService } from './services/course.service';
+import { CourseController } from './controllers/course.controller';
 
-@Module({})
+@Module({
+  providers: [CourseService],
+  controllers: [CourseController],
+})
 export class CourseModule {}

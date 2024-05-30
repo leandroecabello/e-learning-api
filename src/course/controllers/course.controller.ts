1ff6825c6a10c4cf77c7d29a12805118faf07a48
leandro <leandro.ecabello@gmail.com>
2024-05-30 02:51:36 -0300

    Added services and controllers for course

--- src/course/controllers/course.controller.ts
+++ src/course/controllers/course.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CourseService } from '../services/course.service';
+import { Course } from '../entities/course.entity';
 
-@Controller('course')
-export class CourseController {}
+@Controller('courses')
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get()
+  findAll(): Promise<Course[]> {
+    return this.courseService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Course> {
+    return this.courseService.findOne(+id);
+  }
+
+  /**
+   * TODO: Added additional endpoints...
+   */
+}
--- src/course/services/course.service.ts
+++ src/course/services/course.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from '../entities/course.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
+
+  findAll(): Promise<Course[]> {
+    return this.courseRepository.find({ relations: ['lessons'] });
+  }
+
+  findOne(id: number): Promise<Course> {
+    return this.courseRepository.findOne({
+      where: { id },
+      relations: ['lessons'],
+    });
+  }
+
+  /**
+   * TODO: Added additional services...
+   */
+}

9794c496f694a70406081d3cbb47fb86ee115351
leandro <leandro.ecabello@gmail.com>
2024-05-30 01:47:09 -0300

    Added modules course, lesson and question

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  imports: [CourseModule, LessonModule, QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
